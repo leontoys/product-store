629da441e6b02eb19a798f9683da0d9cb661d561
leontoys <svliyon@gmail.com>
2025-03-17 07:23:26 +1100

    product-schema-model-created

---
+++ models/product.model.js
@@ -0,0 +1,23 @@
+import exp from "constants"
+import mongoose from "mongoose"
+
+//schema 
+const ProductSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+    },
+    price : {
+        type : Number,
+        required : true,
+    },
+    image : {
+        type : String,
+        required : true,
+    }
+})
+
+//model
+const Product = mongoose.model("Product",ProductSchema)
+
+export default Product
